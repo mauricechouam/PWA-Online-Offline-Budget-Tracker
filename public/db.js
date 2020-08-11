908550621057a1e3ddfe2f37de03d8052a5826ba
mauricechouam <lansichouamou@gmail.com>
2020-08-11 15:52:25 -0700

    updated db.js

--- public/db.js
+++ public/db.js
@@ -1,5 +1,5 @@
 let db;
-const request = indexedDB.open("heroku_1rh6ds4r", 1);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
   const db = event.target.result;
